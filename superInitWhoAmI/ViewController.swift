a731284bb013001190cd64ccd7fa0706b1557011
mauslenter <marcela.auslenter@kinandcarta.com>
2020-09-02 18:26:04 +0100

    Initial Commit

---
+++ superInitWhoAmI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  superInitWhoAmI
+//
+//  Created by Marcela Auslenter on 02/09/2020.
+//  Copyright © 2020 The App Business. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
